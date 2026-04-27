dfa20b4931eb9e2632f4ff4d029d02876fd217dd
TharushiMedhani <tharushimedhani123@gmail.com>
2026-04-27 18:27:43 +0530

    Update URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = "https://reviewmanager-production-f157.up.railway.app/api/reviews"
 
 function App() {
   const [reviews, setReviews] = useState([]);
